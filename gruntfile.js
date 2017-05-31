bffe73c6d9485900462fde5d6077d56a7504d2e5
Jyoti Kaustuv Nanda <jnanda@visa.com>
2017-05-31 11:58:24 -0700

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    githooks: {
+      all: {
+        'pre-commit': 'test'
+      }
+    }
+  });
+
+  //Load NPM Taks
+  grunt.loadNpmTasks('grunt-githooks');
+
+  //Register tasks
+  
+};
